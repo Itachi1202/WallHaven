093e09ecf45137d79e2e58d44ccacbe0ffbd8297
sree valabh <amadeusrobot.notryuk@gmail.com>
2021-11-23 19:28:13 +0530

    Moved from tracker file to mongodb

--- WallHavenBot/__init__.py
+++ WallHavenBot/__init__.py
@@ -2,13 +2,16 @@ import os
 from dotenv import load_dotenv
 from telegram.ext import Updater
 from telegram import Bot
+from pymongo import MongoClient
 
 load_dotenv()
 
 TOKEN = str(os.environ.get("TOKEN", None))
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", 1214585391))
 API_KEY = str(os.environ.get("API_KEY"))
+DB_URL = str(os.environ.get("DB_URL"))
 
 bot = Bot(TOKEN)
+db_client = MongoClient(DB_URL)
 updater = Updater(TOKEN)
 dispatcher = updater.dispatcher
--- WallHavenBot/tracker.py
+++ WallHavenBot/tracker.py
@@ -1,15 +1,15 @@
+from pymongo.collection import Collection
+from WallHavenBot import db_client
 
 
 class PostTracker:
 
     def __init__(self) -> None:
-        self.file_path = "./WallHavenBot/tracker.txt"
-        self.post_id = open(self.file_path, "r").read()
+        self.col = Collection(db_client["WallHaven"], "tracker")
+        self.post_id = self.col.find_one({"_id": "tracker"})["post_id"]
     
     def update(self, post_id: str):
         try:
-            with open(self.file_path, "w") as tracker:
-                tracker.write(post_id)
-            return post_id
+            return self.col.find_one_and_update({"_id": "tracker"}, {"$set": {"post_id": post_id}})
         except Exception:
             return None
